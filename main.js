bd9b49c2ba7fc7659c7db0dab4bbcf51ce1e765b
NBVTien <149262173+NBVTien@users.noreply.github.com>
2024-01-16 02:34:48 +0700

    Using button for game interaction

--- main.js
+++ main.js
@@ -1,8 +1,33 @@
+const state = {
+    playerScore: 0,
+    computerScore: 0,
+    isEnded: false
+}
+
 const Choices = {
     ROCK: 'rock',
     PAPER: 'paper',
     SCISSORS: 'scissors'
 };
+
+const playerScore = document.querySelector("#player-score");
+const computerScore = document.querySelector("#computer-score");
+const rockButton = document.querySelector("#rock-button");
+const paperButton = document.querySelector("#paper-button");
+const scissorsButton = document.querySelector("#scissors-button");
+const message = document.querySelector("#message");
+
+rockButton.addEventListener("click", () => {
+    playOption(Choices.ROCK)
+});
+
+paperButton.addEventListener("click", () => {
+    playOption(Choices.PAPER)
+});
+
+scissorsButton.addEventListener("click", () => {
+    playOption(Choices.SCISSORS)
+});
 
 function getComputerChoice() {
     const choices = Object.values(Choices);
@@ -24,12 +49,34 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-function game() {    
-    for (let i = 0; i < 5; i++) {
-        const playerSelection = Choices.ROCK;
-        const computerSelection = getComputerChoice();
-        console.log(playRound(playerSelection, computerSelection));
+function playOption(playerSelection) {
+    if (state.isEnded) return;
+
+    const computerSelection = getComputerChoice();
+    const result = playRound(playerSelection, computerSelection); 
+    console.log(result);
+
+    if (result === 1) {
+        message.textContent = "You get a point!";
+        state.playerScore++;
+        playerScore.textContent = state.playerScore.toString();
+        if (state.playerScore === 5) {
+            message.textContent = "You wins!";
+            state.isEnded = true;
+        }
+    }
+
+    if (result === -1) {
+        message.textContent = "Computer gets a point!";
+        state.computerScore++;
+        computerScore.textContent = state.computerScore.toString();
+        if (state.computerScore === 5) {
+            message.textContent = "Computer wins!";
+            state.isEnded = true;
+        }
+    }
+
+    if (result === 0) {
+        message.textContent = "Ties!";
     }
 }
-
-game()
